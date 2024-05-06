abc3d5d37a157fa6bcc55b0c7923a5a38d1c7696
Leonardo Campana <leonardobcampana@gmail.com>
2024-05-06 15:57:26 -0300

    add comentarios e info sobre ddd

--- PesquisarCEP.cs
+++ PesquisarCEP.cs
@@ -1,47 +1,54 @@
 ﻿using Newtonsoft.Json;
 using System;
 using System.Net.Http;
-using System.Runtime.CompilerServices;
-using System.Security.Cryptography;
 using System.Threading.Tasks;
-
-
 
 namespace InfosCEP
 {
+
+    //Classe responsável por realizar consultas de CEP utilizando a API ViaCEP.
+
     public class PesquisarCEP
     {
         private readonly HttpClient _httpClient;
 
+        // Construtor da classe PesquisarCEP.
         public PesquisarCEP()
         {
             _httpClient = new HttpClient();
         }
 
+
+        // Consulta o endereço correspondente ao CEP fornecido utilizando a API ViaCEP.
+
         public async Task<InfosViaService> ConsultarCEP(string cep)
         {
+            // Constrói a URL da API ViaCEP com o CEP fornecido
             string url = $"https://viacep.com.br/ws/{cep}/json/";
 
             try
             {
+                // Realiza uma solicitação GET à API ViaCEP
                 HttpResponseMessage response = await _httpClient.GetAsync(url);
 
-                if(response.IsSuccessStatusCode)
+                // Verifica se a solicitação foi bem sucedida
+                if (response.IsSuccessStatusCode)
                 {
+                    // Lê e desserializa a resposta JSON
                     string jsonResponse = await response.Content.ReadAsStringAsync();
                     return JsonConvert.DeserializeObject<InfosViaService>(jsonResponse);
                 }
                 else
                 {
+                    // Se a solicitação não foi bem sucedida, lança uma exceção com o código de status HTTP
                     throw new Exception($"Erro ao fazer a solicitação: {response.StatusCode}");
                 }
-
             }
-            catch (Exception ex) 
+            catch (Exception ex)
             {
+                // Em caso de erro, lança uma exceção com a mensagem de erro original
                 throw new Exception(ex.Message);
             }
         }
     }
-
 }
